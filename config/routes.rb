daa406906d1400c7ff45c68cd0ae3e2cb2d3125a
keisuke-ksk-git <smoking.bar.ksk@gmail.com>
2019-12-26 20:25:35 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get '/user', to: 'users#index'
+  get '/homes/index', to: 'homes#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
